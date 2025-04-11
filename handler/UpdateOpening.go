89b731a46e416edb88feb7d4e4814dc4ec8f0771
Heloysa <heloysagama2@gmail.com>
2025-04-11 09:39:47 -0300

    feat(handler) adicionando funcionalidades

---
+++ handler/UpdateOpening.go
@@ -0,0 +1,15 @@
+package handler
+
+import (
+	 "github.com/gin-gonic/gin" 
+	  "net/http"
+)	
+
+
+
+func UpdateOpeningHandler (ctx *gin.Context){
+	ctx.JSON(http.StatusOK, gin.H {
+		"message":"GET",
+	})
+}
+
--- handler/handler.go
+++ handler/handler.go
@@ -1,40 +1,6 @@
 package handler
 
-import (
-	 "github.com/gin-gonic/gin" 
-	  "net/http"
-)	
 
 
 
-func CreateOpeningHandler (ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H {
-		"message":"GET",
-	})
-}
-
-func ShowOpeningHandler (ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H {
-		"message":"GET",
-	})
-}
-
-func UpdateOpeningHandler (ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H {
-		"message":"GET",
-	})
-}
-
-
-func DeleteOpeningHandler (ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H {
-		"message":"GET",
-	})
-}
-
-func ListOpeningHandler (ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H {
-		"message":"GET",
-	})
-}
 
