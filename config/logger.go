71392ff77693eb759df1d1e6378a917b95f0ab00
Heloysa <heloysagama2@gmail.com>
2025-04-11 10:06:54 -0300

    add(logs) add formated helpers

--- config/logger.go
+++ config/logger.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"go/format"
 	"io"
 	"log"
 	"os"
@@ -43,4 +44,23 @@ func (l *Logger) Warning(v ...interface{}){
 
 func (l *Logger) Error(v ...interface{}){
 	l.err.Println(v...)
+}
+
+
+//Formated
+
+func (l *Logger) Debugf(format string, v ...interface{}){
+	l.debug.Printf(format, v...)
+}
+
+func (l *Logger) Infof(format string, v ...interface{}){
+	l.info.Printf(format, v...)
+}
+
+func (l *Logger) Warningf(format string, v ...interface{}){
+	l.warning.Printf(format, v...)
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}){
+	l.err.Printf(format, v...)
 }
