c313d9b6fbbc8a250b0afed771259528ccbff206
Heloysa <heloysagama2@gmail.com>
2025-04-11 09:27:18 -0300

    feat(handler) atualizando handler

--- router/routes.go
+++ router/routes.go
@@ -1,37 +1,19 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/helxysa/goportunities.git/handler"
 )
 
 func initializeRoutes(router *gin.Engine){
 	v1 := router.Group("/api/v1/")
 	{
-		v1.GET("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H {
-				"message":"GET",
-			})
-		})
-		v1.POST("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H {
-				"message":"POST",
-			})
-		})
-		v1.PUT("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H {
-				"message":"PUT",
-			})
-		})
-		v1.DELETE("/opening", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H {
-				"message":"DELETE",
-			})
-		})
-		v1.GET("/openings", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H {
-				"message":"teste",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
+		v1.GET("/openings", handler.ListOpeningHandler)
 	}
 }
