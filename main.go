32a61ce9bc11cbdf5a658d4270acdabdbac9160d
Heloysa <heloysagama2@gmail.com>
2025-04-11 10:06:05 -0300

    add(logs) add helpers

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	 "fmt"
 	 "github.com/helxysa/goportunities.git/router"
+	 "github.com/helxysa/goportunities.git/config"
 )
 
 func main () {
+
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+
 	router.Initialize()
 	
 }
